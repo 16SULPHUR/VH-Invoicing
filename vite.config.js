8ae691b39041d5bbd314811abcafc8f47b56265a
“Ankit <22amtics124@gmail.com>
2024-11-19 11:25:13 +0530

    converter to pwa

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 import path from "path"
+import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugIn } from "./manifest";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),VitePWA(manifestForPlugIn)],
   css: {
     postcss: {
       plugins: [tailwindcss()],
